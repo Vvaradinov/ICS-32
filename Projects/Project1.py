fb42c2dacece0947cdecb8f7bf3f0df61d41d01c
Vladislav Nikolaev Varadinov <vladislav.varadinov@gmail.com>
2016-01-11 22:10:03 -0800

    Project 1

--- Projects/Project1.py
+++ Projects/Project1.py
@@ -1,27 +1,35 @@
 import os
 import fnmatch
-
+import shutil
 
 
 def Search():
-    #first_input = input("Enter a path to a file:")
-    #check = os.listdir(first_input)
-   # second_input = input("")
-
+    first_input = input()
     try:
-        first_input = input("Enter a path to a file:")
-        (os.listdir(first_input))
+        os.listdir(first_input)
     except FileNotFoundError:
         print("Error!")
     second_input = input().split()
-    if second_input[0] == "E":
-        print(second_input[1])
-
-
-
-
-
-
-
-
-Search()
+    interesting_files_list = []
+    for file in os.listdir(first_input):
+        pathname = os.path.join(first_input,file)
+        if second_input[0] == "E" and file.endswith(second_input[1]):
+            interesting_files_list.append(file)
+        elif second_input[0] == "N" and fnmatch.fnmatch(file, second_input[1]):
+            interesting_files_list.append(file)
+        elif second_input[0] == "S" and os.stat(pathname).st_size > int(second_input[1]):
+            interesting_files_list.append(file)
+    third_input = input()
+    for obj in interesting_files_list:
+        pathname_2 = os.path.join(first_input,obj)
+        if third_input == "P":
+            print(os.path.abspath(obj))
+        elif third_input == "F":
+            infile = open(pathname_2,"r").readline()
+            print(infile)
+        elif third_input == "D":
+            duplicate = os.path.join(first_input,obj + ".dup")
+            print(shutil.copy(pathname_2,duplicate))
+        elif third_input == "T":
+            os.utime(pathname_2)
+(Search())
